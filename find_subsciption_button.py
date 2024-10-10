b0f0b545c005a35d334b1233b2967a231d82b9be
CSHRIN <98889371+OMG0718@users.noreply.github.com>
2024-10-10 10:40:29 +0800

    Update find_subsciption_button.py

--- find_subsciption_button.py
+++ find_subsciption_button.py
@@ -4,20 +4,22 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+
 with open('your_csv_file.csv','r', encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file)
     service = Service(executable_path=r"path_to_your_chromedriver")  #make sure your chrome version is same as your driver version
     options = webdriver.ChromeOptions()
+    #Please login your chrome account before run this code
     options.add_argument(r"user-data-dir=C:\Users\Your_user_name\AppData\Local\Google\Chrome\User Data") #here need to change to your one
     driver = webdriver.Chrome(service=service, options=options)
 
     for line in csv_reader:
-        url=line[1]
-
+        url=line[1]  #based on the row on your csv file
         print(url)
         driver.get(url)  #open the url
 
         time.sleep(3)    #in seconds
+        """If the action you want to do have ads, maybe u can longer the time sleep to 30seconds"""
         
         xpath = '//*[@id="page-header"]/yt-page-header-renderer/yt-page-header-view-model/div/div[1]/div/yt-flexible-actions-view-model/div/yt-subscribe-button-view-model/yt-animated-action/div[1]/div[2]/button/yt-touch-feedback-shape/div/div[2]'
         #you can copy any xpath for your usage
@@ -26,4 +28,4 @@ with open('your_csv_file.csv','r', encoding="utf8") as csv_file:
         #find "subscribe" button
         subscribe.click()
 
-        time.sleep(3)
+        time.sleep(3)    #in seconds
